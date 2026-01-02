e9524665e6533d9736b364f0da8faefd3100c9b6
Alan <alanmathew190@gmail.com>
2026-01-02 19:23:38 +0530

    cleanup

--- frontend/src/components/RootRedirect.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-
-export default function RootRedirect() {
-  const { authTokens } = useContext(AuthContext);
-
-  return authTokens ? <Navigate to="/home" /> : <Navigate to="/login" />;
-}
